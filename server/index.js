58da537d7eace3edc1ac7257d8658e74817ff01d
Brian Price <brianprice88@gmail.com>
2020-04-18 23:03:10 -0400

    "heroku"

--- server/index.js
+++ server/index.js
@@ -9,9 +9,9 @@ const port = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
-    app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
-    })
+    // app.get('*', function (req, res) {
+    //     res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
+    // })
 }
 
 var rooms = {};

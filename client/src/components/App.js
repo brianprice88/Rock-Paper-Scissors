95a0227d1f953d9ccbf203739878d308bbc94d3e
Brian Price <brianprice88@gmail.com>
2020-04-18 22:11:48 -0400

    "push to Heroku"

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,7 +4,8 @@ import Topbar from './static/Topbar.js';
 import Error from './static/Error.js';
 import Lobby from './Lobby.js';
 import Board from './Board.js';
-const socket = socketIOClient("http://localhost:3001")
+const socket = socketIOClient('https://rock-paper-scissors-brianprice.herokuapp.com/')
+// const socket = socketIOClient("http://localhost:3001")
 
 export default class App extends React.Component {
   constructor(props) {

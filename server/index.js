ffeb854b5475686a59af61ce5f09ac4bbff62417
Brian Price <brianprice88@gmail.com>
2020-04-18 23:00:08 -0400

    Revert ""heroku""
    This reverts commit d9d649cf8f6ba2d6d3eca22dac834816ba45e267.

--- server/index.js
+++ server/index.js
@@ -8,11 +8,10 @@ const io = require('socket.io')(server);
 const port = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === 'production') {
-    app.get('*', (req, res) => res.send('hello world'))
-    // app.use(express.static(path.join(__dirname, '../client/build')));
-    // app.get('*', function (req, res) {
-    //     res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
-    // })
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
+    })
 }
 
 var rooms = {};
